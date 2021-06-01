c7302329abf2599bfa9b85e39598d172d7eb831d
NASSRI Hamdi <nassrihamdi@gmail.com>
2021-06-01 16:48:33 +0200

    soucis auth réglé

--- api/controller/loginController.js
+++ api/controller/loginController.js
@@ -2,9 +2,9 @@ import User from '../model/userModel.js'
 import asyncHandler from 'express-async-handler'
 
 export const loginUser = asyncHandler(async(req, res) => {
-
-    const login1=await User.findOne({ login: req.body.login });
-    if (login1.login===req.body.login , login1.password===req.body.password) {
+console.log(req.body.username)
+    const login1=await User.findOne({ username: req.body.username });
+    if (login1.username===req.body.username , login1.password===req.body.password) {
         res.setHeader('Content-Type', 'application/json');
         res.send(JSON.stringify({ greeting: `Vous êtes connectés` }));
     }
