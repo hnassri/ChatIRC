9e3b18f8f360697de30654c1429653ed8801351f
Mahamat Abakar <mahamat.abakar-hassan@epitech.eu>
2021-06-13 18:58:31 +0200

    onglet

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -4,13 +4,16 @@ import AuthContext from "../context/auth";
 import VerticalTabs from "../component/Tabs"
 import { Link,Redirect,useHistory  } from "react-router-dom";
 import axios from 'axios';
+import '../App.css';
 const Chat =props => {
     const [message, setMessage] = useState("");
     const [channel, setChannel] = useState("");
+    const [notification, setnotification] = useState("");
     const user = JSON.parse(localStorage.getItem("auth"));
     const channelToUseCallback = (channel) => {
         setChannel(channel);
     };
+
     const handleSubmit = (e) => {
         e.preventDefault();
         switch (message.split(' ')[0]) {
@@ -23,7 +26,16 @@ const Chat =props => {
                     
                     socket.emit('nick', nick),
                     setMessage(""),
-                    e.target.reset()
+                    e.target.reset(),
+                    socket.on("nickname update", function(data) {
+                        if(data.success === "success"){
+                            setnotification("Your nickname has been updated");
+                            user.username = data.username;
+                            localStorage.setItem("auth", JSON.stringify(user));
+                        }else{
+                            setnotification('Update failed');
+                        }
+                    })
                 )
             case '/create':
                 const data = {
@@ -34,7 +46,14 @@ const Chat =props => {
                     
                     socket.emit('create channel', data),
                     setMessage(""),
-                    e.target.reset()
+                    e.target.reset(),
+                    socket.on("add channel", function(msg) {
+                        setnotification("You have create " + msg + " channel");
+                        socket.emit('join', {
+                          channel_name: msg,
+                          user: user
+                      });
+                    })
                 )
             case '/join':
                 const dataJoin= {
@@ -44,7 +63,14 @@ const Chat =props => {
                 return(
                     socket.emit('join', dataJoin),
                     setMessage(""),
-                    e.target.reset()
+                    e.target.reset(),
+                    socket.on("joinChannel", function(data) {
+                        if(data.success === "success"){
+                          setnotification(" You are join channel " + data.channel_name);
+                        }else{
+                            setnotification('Update ggg failed');
+                        }
+                    })
                 )
             case '/leave':
                 const dataLeave= {
@@ -54,7 +80,14 @@ const Chat =props => {
                 return(
                     socket.emit('leave', dataLeave),
                     setMessage(""),
-                    e.target.reset()
+                    e.target.reset(),
+                    socket.on("leaveChannel", function(data) {
+                        if(data.success === "success"){
+                            setnotification("You are leave channel " + data.channel_name);
+                        }else{
+                            setnotification("Channel don't exist");
+                        }
+                       })
                 )
             case '/delete':
             const dataDelete= {
@@ -64,7 +97,14 @@ const Chat =props => {
             return(
                 socket.emit('delete', dataDelete),
                 setMessage(""),
-                e.target.reset()
+                e.target.reset(),
+                socket.on("deleteChannel", function(data) {
+                    if(data.success === "success"){
+                        setnotification("You are delete channel " + data);
+                    }else{
+                        setnotification("Channel don't exist");
+                    }
+                })
             )
             case '/users':
                 const dataUsers= {
@@ -82,7 +122,14 @@ const Chat =props => {
                 return(
                     socket.emit('list', dataList),
                     setMessage(""),
-                    e.target.reset()
+                    e.target.reset(),
+                    socket.on("listChannel", function(data) {
+                        if(data.success === "success"){
+                            setnotification("channel " + data.channels);
+                        }else{
+                            setnotification("we have. no channel with this name");
+                        }
+                    })
                 )
             case '/msg':
             const datamsg= {
@@ -92,7 +139,14 @@ const Chat =props => {
             return(
                 socket.emit('msg', datamsg),
                 setMessage(""),
-                e.target.reset()
+                e.target.reset(),
+                socket.on("privatemsg", function(data) {
+                    if(data.success === "success"){
+                        setnotification("msg send");
+                    }else{
+                        setnotification("msg not send");
+                    }
+                })
             )
             default:
                 if(message !== "" && channel !== ""){ 
@@ -109,6 +163,7 @@ const Chat =props => {
         return <Redirect to="/login" />
     }
     return (
+        <div>
         <AuthContext.Provider value={user}>
             <SocketContext.Provider value={socket}>
                 <div className="Chat">
@@ -117,10 +172,13 @@ const Chat =props => {
                         <input onChange={event => setMessage(event.target.value)} placeholder="Entrez votre message" /><button>Send</button>
                     </form>
                 </div>
-                <div>
+                <div className="onglet">
+                    <p>{notification}</p>
                 </div>
             </SocketContext.Provider>
         </AuthContext.Provider>
+        </div>
+        
 
     )
 }
